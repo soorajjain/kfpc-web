a55fc20ae25f1c830aab0e096704d290c6fec61f
Sooraj Jain <soorajjain51@gmail.com>
2025-09-28 20:34:06 +0530

    Host: gitHub pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/kfpc-web/",
   resolve: {
     alias: {
       '@layout': path.resolve(__dirname, 'src/layout.js'),
